f03d8858d489aa98f00462533697964484e9a084
hoangkhang0267-coder <hoangkhang0267@gmail.com>
2026-07-03 23:47:16 +0700

    thêm đăng nhập

--- Main/MiniSocialApp/login.Designer.cs
+++ Main/MiniSocialApp/login.Designer.cs
@@ -28,47 +28,168 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnLogin = new System.Windows.Forms.Button();
-            this.txtName = new System.Windows.Forms.TextBox();
-            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.logintext2 = new MiniSocialApp.logintext();
+            this.logintext1 = new MiniSocialApp.logintext();
+            this.gradientPanel1 = new MiniSocialApp.LoginUI_Design.GradientPanel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.gradientPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
-            // btnLogin
+            // label1
             // 
-            this.btnLogin.Location = new System.Drawing.Point(144, 208);
-            this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(226, 76);
-            this.btnLogin.TabIndex = 0;
-            this.btnLogin.Text = "btnLogin";
-            this.btnLogin.UseVisualStyleBackColor = true;
-            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(658, 295);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Forgot your password?";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // txtName
+            // button1
             // 
-            this.txtName.Location = new System.Drawing.Point(460, 139);
-            this.txtName.Multiline = true;
-            this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(476, 50);
-            this.txtName.TabIndex = 1;
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(470, 332);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(332, 50);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // txtPhone
+            // button2
             // 
-            this.txtPhone.Location = new System.Drawing.Point(460, 373);
-            this.txtPhone.Multiline = true;
-            this.txtPhone.Name = "txtPhone";
-            this.txtPhone.Size = new System.Drawing.Size(476, 50);
-            this.txtPhone.TabIndex = 2;
+            this.button2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(470, 400);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(332, 50);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Login with Google";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(441, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(372, 29);
+            this.label2.TabIndex = 7;
+            this.label2.Text = " SIGN IN TO YOUR ACCOUNT";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(523, 469);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(146, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Don\'t have an account?";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Blue;
+            this.label5.Location = new System.Drawing.Point(683, 469);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Sign up";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // logintext2
+            // 
+            this.logintext2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(145)))), ((int)(((byte)(245)))));
+            this.logintext2.IsPassword = true;
+            this.logintext2.Location = new System.Drawing.Point(470, 216);
+            this.logintext2.Name = "logintext2";
+            this.logintext2.Padding = new System.Windows.Forms.Padding(0, 0, 0, 1);
+            this.logintext2.Size = new System.Drawing.Size(332, 59);
+            this.logintext2.TabIndex = 2;
+            this.logintext2.TitleText = "Password";
+            // 
+            // logintext1
+            // 
+            this.logintext1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(145)))), ((int)(((byte)(245)))));
+            this.logintext1.IsPassword = false;
+            this.logintext1.Location = new System.Drawing.Point(470, 120);
+            this.logintext1.Name = "logintext1";
+            this.logintext1.Padding = new System.Windows.Forms.Padding(0, 0, 0, 1);
+            this.logintext1.Size = new System.Drawing.Size(332, 59);
+            this.logintext1.TabIndex = 1;
+            this.logintext1.TitleText = "Email";
+            // 
+            // gradientPanel1
+            // 
+            this.gradientPanel1.Controls.Add(this.label3);
+            this.gradientPanel1.Controls.Add(this.pictureBox2);
+            this.gradientPanel1.gradientBottom = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(145)))), ((int)(((byte)(245)))));
+            this.gradientPanel1.gradientTop = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(74)))), ((int)(((byte)(158)))));
+            this.gradientPanel1.Location = new System.Drawing.Point(-3, -6);
+            this.gradientPanel1.Name = "gradientPanel1";
+            this.gradientPanel1.Size = new System.Drawing.Size(438, 505);
+            this.gradientPanel1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(94, 202);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(176, 36);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "WELCOME";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::MiniSocialApp.Properties.Resources.cloud;
+            this.pictureBox2.Location = new System.Drawing.Point(274, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(163, 499);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
             // 
             // login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1550, 765);
-            this.Controls.Add(this.txtPhone);
-            this.Controls.Add(this.txtName);
-            this.Controls.Add(this.btnLogin);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(834, 494);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.logintext2);
+            this.Controls.Add(this.logintext1);
+            this.Controls.Add(this.gradientPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "login";
-            this.Text = "login";
+            this.Text = "Form1";
+            this.gradientPanel1.ResumeLayout(false);
+            this.gradientPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -76,8 +197,18 @@
 
         #endregion
 
-        private System.Windows.Forms.Button btnLogin;
-        private System.Windows.Forms.TextBox txtName;
-        private System.Windows.Forms.TextBox txtPhone;
+        private LoginUI_Design.GradientPanel gradientPanel1;
+        private logintext logintext1;
+        private logintext logintext2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
+
--- Main/MiniSocialApp/login.cs
+++ Main/MiniSocialApp/login.cs
@@ -1,72 +1,315 @@
-using Google.Cloud.Firestore;
-using MiniSocialApp.Services;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿using System;
+using System.Diagnostics;
 using System.Drawing;
-using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Firebase.Auth;
+using Firebase.Auth.Providers;
+using MiniSocialApp.Services;
+using System.Runtime.InteropServices;
+using MiniSocialApp.Config;
+using MiniSocialApp.Services;
 
 namespace MiniSocialApp
 {
     public partial class login : Form
     {
+        [DllImport("user32.dll")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        private const int WM_NCLBUTTONDOWN = 0xA1;
+        private const int HT_CAPTION = 0x2;
+
+        private void Form_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private HttpListener listener;
+
         public login()
         {
             InitializeComponent();
+            this.MouseDown += Form_MouseDown;
         }
 
-        private async void btnLogin_Click(object sender, EventArgs e)
+        private async void button1_Click(object sender, EventArgs e)
         {
-            string name = txtName.Text.Trim();
-            string phone = txtPhone.Text.Trim();
+            var authClient = FirebaseService.GetAuthClient();
+            string email = logintext1.Text.Trim();
+            string password = logintext2.Text.Trim();
 
-            if (string.IsNullOrEmpty(name))
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
             {
-                MessageBox.Show("Nhập tên đi bro");
+                ShowError("Vui lòng nhập đầy đủ Email và Mật khẩu!");
                 return;
             }
-
-            // ✅ FIX 12: Validate số điện thoại
-            if (!string.IsNullOrEmpty(phone) && !System.Text.RegularExpressions.Regex.IsMatch(phone, @"^\d{9,11}$"))
-            {
-                MessageBox.Show("Số điện thoại không hợp lệ (9–11 chữ số).");
-                return;
-            }
-
-            btnLogin.Enabled = false;
 
             try
             {
-                var userService = new UserService(new FirestoreContext());
-                var user = await userService.LoginOrCreate(name, phone);
+                var userCredential = await authClient.SignInWithEmailAndPasswordAsync(email, password);
 
-                // lưu current user
-                CurrentUserStore.User = user;
+                MessageBox.Show(this, "Đăng nhập thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
-                // mở main form
-                var main = new Form1();
-                // Khi Form1 đóng (không phải logout) → đóng luôn login form để thoát app
-                main.FormClosed += (s2, e2) =>
+                await OpenMainAppAfterLogin(
+                    Convert.ToString(userCredential.User.Uid),
+                    Convert.ToString(userCredential.User.Info.Email),
+                    Convert.ToString(userCredential.User.Info.DisplayName),
+                    Convert.ToString(userCredential.User.Info.PhotoUrl)
+                );
+            }
+            catch (FirebaseAuthException ex)
+            {
+                string friendlyMessage = "Đã xảy ra lỗi, vui lòng thử lại sau.";
+                string rawError = ex.Message.ToLower();
+
+                if (rawError.Contains("invalid_email") || rawError.Contains("invalid-email"))
                 {
-                    if (!main.IsRestarting)
-                        this.Close(); // đóng main form → thoát app
-                };
-                main.Show();
+                    friendlyMessage = "Email không đúng định dạng!";
+                }
+                else if (rawError.Contains("invalid_login_credentials") ||
+                         rawError.Contains("invalid_password") ||
+                         rawError.Contains("user_not_found"))
+                {
+                    friendlyMessage = "Email hoặc mật khẩu không chính xác.";
+                }
+                else if (rawError.Contains("too_many_attempts"))
+                {
+                    friendlyMessage = "Bạn đã thử quá nhiều lần. Vui lòng thử lại sau!";
+                }
 
-                // Ẩn login form (không Close vì login là main form của Application.Run)
-                this.Hide();
+                ShowError(friendlyMessage);
+            }
+            catch (Exception ex) { ShowError("Lỗi hệ thống: " + ex.Message); }
+        }
+
+        private void ShowError(string msg)
+        {
+            MessageBox.Show(this, msg, "Lỗi xác thực", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            this.Activate();
+        }
+
+        private async void label1_Click(object sender, EventArgs e)
+        {
+            if (!label1.Enabled) return;
+            var authClient = FirebaseService.GetAuthClient();
+            string email = logintext1.Text.Trim();
+
+            if (string.IsNullOrEmpty(email))
+            {
+                MessageBox.Show(this, "Hãy nhập Email!", "Nhắc nhở", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                await authClient.ResetEmailPasswordAsync(email);
+                MessageBox.Show(this, $"Đã gửi link reset mật khẩu tới {email}, vui lòng kiểm tra mục spam.", "Thành công", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                label1.Enabled = false;
+                string originalText = label1.Text;
+                for (int i = 10; i > 0; i--)
+                {
+                    label1.Text = $"Thử lại sau {i}s";
+                    await Task.Delay(1000);
+                }
+                label1.Text = originalText;
+                label1.Enabled = true;
             }
             catch (Exception ex)
             {
-                // ✅ FIX 3: Xử lý lỗi mạng/Firestore – re-enable nút login
-                MessageBox.Show("Không thể đăng nhập. Kiểm tra kết nối mạng.\n\nChi tiết: " + ex.Message,
-                                "Đăng nhập thất bại", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                btnLogin.Enabled = true;
+                ShowError("Lỗi: " + ex.Message);
+                label1.Enabled = true;
             }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            register registerForm = new register();
+            registerForm.Show();
+            this.Hide();
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            // Dùng port 8081 cho đồng bộ với bên Register
+            string redirectUri = "http://localhost:8081/";
+            string url = $"https://accounts.google.com/o/oauth2/auth?client_id={FirebaseService.GoogleClientId}&redirect_uri={redirectUri}&scope=email%20profile&response_type=code";
+
+            try
+            {
+                if (listener == null || !listener.IsListening)
+                {
+                    listener = new HttpListener();
+                    listener.Prefixes.Add(redirectUri);
+                    listener.Start();
+                }
+
+                Process.Start(url);
+
+                HttpListenerContext context = await listener.GetContextAsync();
+                string code = context.Request.QueryString.Get("code");
+
+                string responseString = $@"
+<html>
+<head>
+    <meta charset='utf-8'>
+    <style>
+        body {{ 
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; 
+            background-color: #f0f2f5; 
+            display: flex; 
+            justify-content: center; 
+            align-items: center; 
+            height: 100vh; 
+            margin: 0; 
+        }}
+        .card {{ 
+            background: white; 
+            padding: 40px; 
+            border-radius: 12px; 
+            box-shadow: 0 10px 25px rgba(0,0,0,0.1); 
+            text-align: center; 
+            max-width: 400px; 
+            animation: fadeIn 0.6s ease-out;
+        }}
+        .icon-circle {{
+            width: 80px;
+            height: 80px;
+            background-color: #4BB543;
+            border-radius: 50%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 0 auto 20px;
+            color: white;
+            font-size: 40px;
+            box-shadow: 0 4px 10px rgba(75, 181, 67, 0.3);
+        }}
+        h1 {{ color: #1877f2; margin: 0 0 10px 0; font-size: 24px; }}
+        p {{ color: #65676b; font-size: 16px; margin: 0; }}
+        .loader {{
+            margin-top: 25px;
+            height: 4px;
+            width: 100%;
+            background: #e4e6eb;
+            border-radius: 2px;
+            overflow: hidden;
+            position: relative;
+        }}
+        .loader::after {{
+            content: '';
+            position: absolute;
+            left: 0; height: 100%; width: 0;
+            background: #1877f2;
+            animation: progress 2s linear forwards;
+        }}
+        @keyframes fadeIn {{ from {{ opacity: 0; transform: translateY(20px); }} to {{ opacity: 1; transform: translateY(0); }} }}
+        @keyframes progress {{ from {{ width: 0; }} to {{ width: 100%; }} }}
+    </style>
+</head>
+<body>
+    <div class='card'>
+        <div class='icon-circle'>✓</div>
+        <h1>Koobecaf Verified</h1>
+        <p>Xác thực thành công! Hệ thống đang chuyển hướng...</p>
+        <div class='loader'></div>
+        <p style='font-size: 12px; margin-top: 15px; color: #bcc0c4;'>Cửa sổ này sẽ tự động đóng sau giây lát.</p>
+    </div>
+    <script>
+        setTimeout(function(){{ window.close(); }}, 2000);
+    </script>
+</body>
+</html>";
+
+                byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+                context.Response.ContentLength64 = buffer.Length;
+                context.Response.OutputStream.Write(buffer, 0, buffer.Length);
+                context.Response.OutputStream.Close();
+
+                listener.Stop();
+
+                if (!string.IsNullOrEmpty(code))
+                {
+                    await LoginWithGoogleCodeAsync(code, redirectUri);
+                }
+            }
+            catch (Exception ex)
+            {
+                if (listener != null) listener.Stop();
+                ShowError("Lỗi kết nối Google: " + ex.Message);
+            }
+        }
+
+        private async Task LoginWithGoogleCodeAsync(string code, string redirectUri)
+        {
+            try
+            {
+                string decodedCode = System.Net.WebUtility.UrlDecode(code);
+                var access = AuthResponse.Exchange(decodedCode,
+                                                  FirebaseService.GoogleClientId,
+                                                  FirebaseService.GoogleClientSecret,
+                                                  redirectUri);
+                var credential = GoogleProvider.GetCredential(access.IdToken, OAuthCredentialTokenType.IdToken);
+
+                var authClient = FirebaseService.GetAuthClient();
+                var userCredential = await authClient.SignInWithCredentialAsync(credential);
+
+                this.Invoke((MethodInvoker)delegate {
+                    this.WindowState = FormWindowState.Normal; // Nếu đang thu nhỏ thì bung lên
+                    SetForegroundWindow(this.Handle);          // Ép Windows đẩy app lên trước mặt
+                    this.Activate();                           // Kích hoạt Form
+                });
+
+                MessageBox.Show(this, "Đăng nhập Google thành công!", "Koobecaf", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                await OpenMainAppAfterLogin(
+                    Convert.ToString(userCredential.User.Uid),
+                    Convert.ToString(userCredential.User.Info.Email),
+                    Convert.ToString(userCredential.User.Info.DisplayName),
+                    Convert.ToString(userCredential.User.Info.PhotoUrl)
+                );
+            }
+            catch (Exception ex) { ShowError("Lỗi xác thực Google: " + ex.Message); }
+        }
+
+        private async Task OpenMainAppAfterLogin(string firebaseUid, string email, string displayName, string photoUrl = "")
+        {
+            var authUserService = new FirebaseAuthUserService(new global::FirestoreContext());
+
+            var user = await authUserService.LoginOrCreateFirebaseUser(
+                firebaseUid,
+                email,
+                displayName,
+                photoUrl
+            );
+
+            CurrentUserStore.User = user;
+
+            var main = new Form1();
+
+            main.FormClosed += (s, e) =>
+            {
+                if (!main.IsRestarting && !this.IsDisposed)
+                {
+                    this.Close();
+                }
+            };
+
+            main.Show();
+            SetForegroundWindow(main.Handle);
+            this.Hide();
         }
     }
 }
