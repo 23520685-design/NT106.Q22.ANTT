15068ae8c9422cbf4b4c2862c786cdb6be1bb265
hoangkhang0267-coder <hoangkhang0267@gmail.com>
2026-07-03 23:46:03 +0700

    Add: thêm control giao diện cho đăng nhập và đăng kí

---
+++ Main/MiniSocialApp/GradientPanel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace MiniSocialApp.LoginUI_Design
+{
+    public class GradientPanel : Panel
+    {
+        public Color gradientTop { get; set; }
+        public Color gradientBottom { get; set; }
+
+        public GradientPanel()
+        {
+            this.Resize += GradientPanel_Resize;
+        }
+
+        private void GradientPanel_Resize(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            using (LinearGradientBrush linear = new LinearGradientBrush(
+                this.ClientRectangle,
+                this.gradientTop,
+                this.gradientBottom,
+                90F))
+            {
+                e.Graphics.FillRectangle(linear, this.ClientRectangle);
+            }
+
+            base.OnPaint(e);
+        }
+    }
+}
